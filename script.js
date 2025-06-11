b3724aaf7d66a17359b5601269416cee7b0b5f2f
dollarwolv <dollarwolv@gmail.com>
2025-06-11 13:48:43 +0200

    Upgrade design (a bit)

--- script.js
+++ script.js
@@ -1,34 +1,64 @@
-const allButtons = document.querySelectorAll('.buttons button');
-var isAnimating = false
+const allButtons = document.querySelectorAll('#buttons button');
+const possibleMoves = ["rock", "paper", "scissors"];
+const winningConditions = {
+    rock: 'scissors',
+    paper: 'rock',
+    scissors: 'paper'
+};
+const imageDict = {
+    "rock" : "img/rock.png",
+    "paper" : "img/paper.png",
+    "scissors" : "img/scissors.png"
+};
+
+var isAnimating = false;
+var playerScore = 0
+var computerScore = 0
 
 function playAnimation(elementId, onCallback) {
-  var elem = document.getElementById(elementId);
-  let pos = 0;
-  let frameCounter = 0;
+    var elem = document.getElementById(elementId);
+    let pos = 0;
+    let frameCounter = 0;
+    const id = setInterval(frame, 2);
 
-  const id = setInterval(frame, 2);
+    document.getElementById("player-img").src="img/fist.png";
+    document.getElementById("computer-img").src="img/inv_fist.png";
 
-  function frame() {
-    if (frameCounter == 320) {
-      clearInterval(id);
+    function frame() {
+        if (frameCounter == 320) {
+        clearInterval(id);
+        if (onCallback){
+            onCallback();
+            return;
+        }}
 
-      if (onCallback){
-        onCallback();
-        return;
-      }
+        let blockNumber = Math.floor(frameCounter / 40);
+        if (blockNumber % 2 === 0) {
+            pos++;
+        } else {
+            pos--;
+        }
+
+        elem.style.transform = `translateY(-${pos}px)`; 
+        frameCounter++;
+    }
+}
+
+function playRound(playerMove){
+    let i = Math.floor(Math.random() * possibleMoves.length);
+    let computerMove = possibleMoves[i];
+
+    if (winningConditions[playerMove] == computerMove){
+        playerScore++;
+    } else if (winningConditions[computerMove] == playerMove){
+        computerScore++;
     }
 
-    let blockNumber = Math.floor(frameCounter / 40);
-    
-    if (blockNumber % 2 === 0) {
-        pos++;
-    } else {
-        pos--;
-    }
+    document.getElementById("player-img").src=imageDict[playerMove];
+    document.getElementById("computer-img").src=imageDict[computerMove];
 
-    elem.style.transform = `translateY(-${pos}px)`; 
-    frameCounter++;
-  }
+    document.getElementById("player-score").textContent=playerScore;
+    document.getElementById("computer-score").textContent=computerScore;
 }
 
 allButtons.forEach(button => {
@@ -40,26 +70,25 @@ allButtons.forEach(button => {
         }
 
         isAnimating = true
-        
         console.log(this.className + ' button was clicked!'); // 'this' refers to the button that was clicked
-
-        // --- BONUS: VISUALLY DISABLE BUTTONS ---
         allButtons.forEach(btn => {
             btn.disabled = true;
-            // Optional: Change the style to make it look disabled.
             btn.style.opacity = '0.5'; 
         });
+
+        var playerMove = this.className
 
         playAnimation('player-img');
         playAnimation('computer-img', function() {
             console.log("Animation finished!");
             isAnimating = false
+            playRound(playerMove)
 
             allButtons.forEach(btn => {
             btn.disabled = false;
             btn.style.opacity = '0.5'; 
             });
         });
-        
+
     });
 });
